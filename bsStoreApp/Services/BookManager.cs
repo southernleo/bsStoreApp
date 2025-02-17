313c1ea772de5400fcead6e49604111beade1f68
southernleo <southernleo7347@gmail.com>
2025-02-17 14:31:56 +0300

    Error Handling
    Global error handling implemented

--- bsStoreApp/Services/BookManager.cs
+++ bsStoreApp/Services/BookManager.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Repositories.Contracts;
 using Services.Contracts;
 
@@ -32,10 +33,7 @@ namespace Services
             var entity=_manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
             {
-                string message = $"Book with id.{id} could not found";
-                _logger.LogInfo(message);
-                throw new Exception(message);
-
+               throw new  BookNotFoundException(id);
             }
                 
 
@@ -52,7 +50,12 @@ namespace Services
 
         public Book GetOneBookById(int id, bool trackChanges)
         {
-                return _manager.Book.GetOneBookById(id,trackChanges);
+               var book= _manager.Book.GetOneBookById(id,trackChanges);
+            if(book is null)
+            {
+                throw new BookNotFoundException(id);
+            }
+            return book;
         }
 
         public void UpdateOneBook(int id, Book book, bool trackChanges)
--- bsStoreApp/WebApi/Program.cs
+++ bsStoreApp/WebApi/Program.cs
@@ -1,6 +1,7 @@
 
 
 using NLog;
+using Services.Contracts;
 using WebApi.Extensions;
 
 
@@ -21,14 +22,18 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureLoggerService();
 var app = builder.Build();
-
+var logger=app.Services.GetRequiredService<ILoggerService>();
+app.ConfigureExceptionHandler(logger);
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+if (app.Environment.IsProduction())
+{
+    app.UseHsts();
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
