9bf5d5a7bf408befb11b98977cce1faca31a6098
southernleo <southernleo7347@gmail.com>
2025-02-21 16:03:11 +0300

    Create Token

--- bsStoreApp/Services/AuthenticationManager.cs
+++ bsStoreApp/Services/AuthenticationManager.cs
@@ -3,10 +3,13 @@ using Entities.DataTransferObjects;
 using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Services.Contracts;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +21,7 @@ namespace Services
         private readonly IMapper _mapper;
         private readonly UserManager<User> _userManager;
         private readonly IConfiguration _configuration;
+        private User? _user;
 
         public AuthenticationManager(ILoggerService logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
         {
@@ -27,6 +31,56 @@ namespace Services
             _configuration = configuration;
         }
 
+        public async Task<string> CreateToken()
+        {
+            var signinCredentials = GetSinginCredentials();
+            var claims = await GetClaims();
+            var tokenOptions = GenerateTokenOptions(signinCredentials, claims);
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signinCredentials, List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSetting");
+            var tokenOptions = new JwtSecurityToken(
+                issuer: jwtSettings["validIssuer"],
+                audience: jwtSettings["validAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expires"])),
+                signingCredentials: signinCredentials);
+
+
+            return tokenOptions;
+
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,_user.UserName)
+            };
+            var roles = await _userManager
+                .GetRolesAsync(_user);
+            foreach (var role in roles)
+            
+              {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+
+              }
+                return claims;
+        }
+
+        private SigningCredentials GetSinginCredentials()
+        {
+            var jwtSettings = _configuration.GetSection("JwtSetting");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["secretkey"]);
+            var secret=new SymmetricSecurityKey(key);
+            return new SigningCredentials(secret,SecurityAlgorithms.HmacSha256);
+        }
+
         public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto)
         {
             var user=_mapper.Map<User>(userForRegistrationDto);
@@ -34,6 +88,17 @@ namespace Services
             if (result.Succeeded)
             
                 await _userManager.AddToRolesAsync(user, userForRegistrationDto.Roles);
+            return result;
+        }
+
+        public async Task<bool> ValidateUser(UserForAuthenticationDto userForAuthDto)
+        {
+            _user=await _userManager.FindByNameAsync(userForAuthDto.UserName);
+            var result=(_user!=null&&await _userManager.CheckPasswordAsync(_user,userForAuthDto.Password));
+            if (!result)
+            {
+                _logger.LogWarning($"{nameof(ValidateUser)}:Authentication failed.Wrong username or password");
+            }
             return result;
         }
     }
